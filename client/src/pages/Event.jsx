544a0f5665d080a2ba243b37a53d6d60092463d1
sindikoi <sindikoifman@gmail.com>
2025-07-26 02:08:28 +0300

    creating a from

--- client/src/pages/Event.jsx
+++ client/src/pages/Event.jsx
@@ -1,7 +1,25 @@
 import { useNavigate }  from "react-router-dom";
+import {useState} from "react";
 
 function Event() {
     const navigate = useNavigate();
+
+    const [event, setEvent] = useState ({
+        date:"",
+        name:"",
+        kind:"",
+        place:"",
+        numberOfGuests:"",
+        conditions:"",
+    })
+
+    const handlechange = (e) => {
+        const {name, value} = e.darget;
+        setEvent(prev => ({
+            ...prev,
+            [name]: value
+        }));
+    };
 return (
 <div>
     <h2>Event</h2>
